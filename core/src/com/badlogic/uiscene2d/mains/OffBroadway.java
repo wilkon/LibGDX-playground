cea232d60544512895bfdcdb9611e000904afb30
wilkon <tb.wilkon@gmail.com>
2020-04-26 19:56:20 -0400

    UIScene2D - making tiled grid

---
+++ core/src/com/badlogic/uiscene2d/mains/OffBroadway.java
@@ -0,0 +1,58 @@
+package com.badlogic.uiscene2d.mains;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class OffBroadway extends ApplicationAdapter {
+    private Stage stage;
+
+    @Override
+    public void create(){
+        //creating a tiled grid N x N
+        int N = 8;
+
+        createGrid(N);
+    }
+    private void createGrid(int N){
+
+        stage = new Stage(new ScreenViewport());
+        Texture texture = new Texture(Gdx.files.internal("image.jpg"));
+
+        //type of repetition - MirroedRepeat will mirror each to the next
+        texture.setWrap(Texture.TextureWrap.MirroredRepeat, Texture.TextureWrap.MirroredRepeat);
+
+        TextureRegion tiledRegion = new TextureRegion(texture);
+        // how we split our region - in this case, N defines row/col count
+        tiledRegion.setRegion(0, 0, Gdx.graphics.getWidth() * N, Gdx.graphics.getHeight() * N);
+
+        //assigning our tiled region to tiledImage
+        Image tiledImage = new Image(tiledRegion);
+
+        //filling the screen
+        tiledImage.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getWidth());
+
+        //drawing our new grid filled actor!
+        tiledImage.setPosition(0, Gdx.graphics.getHeight() - tiledImage.getHeight());
+        stage.addActor(tiledImage);
+    }
+
+    @Override
+    public void render(){
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void dispose(){
+
+    }
+}
--- desktop/src/com/badlogic/droplets/mains/desktop/DesktopLauncher.java
+++ desktop/src/com/badlogic/droplets/mains/desktop/DesktopLauncher.java
@@ -2,13 +2,13 @@ package com.badlogic.droplets.mains.desktop;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.badlogic.uiscene2d.mains.Broadway;
+import com.badlogic.uiscene2d.mains.OffBroadway;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("MAKING IT RAIN!");
-		config.setWindowSizeLimits(800, 480, 999, 999);
-		new Lwjgl3Application(new Broadway(), config);
+		config.setWindowSizeLimits(1000, 1000, 1000, 1000);
+		new Lwjgl3Application(new OffBroadway(), config);
 	}
 }
