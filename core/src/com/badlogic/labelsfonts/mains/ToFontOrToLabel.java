e26da3a5a1d3c3e955ca8e3ebf76351bbf5def06
wilkon <tb.wilkon@gmail.com>
2020-04-26 23:36:22 -0400

    Labels/Fonts - Labels/BitmapFonts/TrueType/SkinFonts

---
+++ core/src/com/badlogic/labelsfonts/mains/ToFontOrToLabel.java
@@ -0,0 +1,117 @@
+package com.badlogic.labelsfonts.mains;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class ToFontOrToLabel extends ApplicationAdapter {
+    private Stage stage;
+
+    int col_width;
+    int row_height;
+    int help_guide;
+
+    @Override
+    public void create(){
+        stage = new Stage(new ScreenViewport());
+
+        help_guide = 12;
+        row_height = Gdx.graphics.getWidth() / 12;
+        col_width = Gdx.graphics.getWidth() / 12;
+        createGrid(help_guide);
+        addBitmapFont();
+
+//        addTrueType(); not working on desktop
+
+        addSkinFont();
+    }
+
+    @Override
+    public void render(){
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void dispose(){
+
+    }
+
+    private void addSkinFont(){
+        Skin flatEarthSkin = new Skin(Gdx.files.internal("skins/glassy/glassy-ui.json"));
+
+        Label flatLabel = new Label("A very Glassy day to you, good sir", flatEarthSkin, "big");
+        flatLabel.setSize(Gdx.graphics.getWidth()/ help_guide * 6, row_height);
+        flatLabel.setColor(Color.BLACK);
+        flatLabel.setWrap(true);
+        flatLabel.setPosition(col_width * 2, Gdx.graphics.getHeight()-row_height*6);
+        stage.addActor(flatLabel);
+    }
+
+    private void addBitmapFont(){
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        BitmapFont myFont = new BitmapFont(Gdx.files.internal("hacker-writer.fnt"));
+        labelStyle.font = myFont;
+        labelStyle.fontColor = Color.BLACK;
+
+        Label titleLabel = new Label("Title (Michael Scarn)", labelStyle);
+        titleLabel.setSize(Gdx.graphics.getWidth(), row_height);
+        titleLabel.setPosition(0, Gdx.graphics.getHeight() - row_height * 2);
+        titleLabel.setAlignment(Align.center);
+        stage.addActor(titleLabel);
+    }
+
+    private void addTrueType(){
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("Amble-Light.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 30;
+        parameter.borderWidth = 1;
+        parameter.color = Color.YELLOW;
+        parameter.shadowOffsetX = 3;
+        parameter.shadowOffsetY = 3;
+        parameter.shadowColor = new Color(0, 0.5f, 0, 0.75f);
+        BitmapFont ttFont = generator.generateFont(parameter);
+        generator.dispose();
+
+        Label.LabelStyle ambleStyle = new Label.LabelStyle();
+        ambleStyle.font = ttFont;
+
+        Label lambel = new Label("True Types", ambleStyle);
+        lambel.setSize(Gdx.graphics.getWidth()/ help_guide *6, row_height);
+        lambel.setPosition(col_width*2, Gdx.graphics.getHeight()-row_height*4);
+        stage.addActor(lambel);
+    }
+
+    // doing it again for practice
+    private void createGrid(int N){
+        Texture texture = new Texture(Gdx.files.internal("background-blue-tiles.jpg"));
+
+        texture.setWrap(Texture.TextureWrap.MirroredRepeat, Texture.TextureWrap.MirroredRepeat);
+
+        TextureRegion tiledRegion = new TextureRegion(texture);
+        tiledRegion.setRegion(0, 0, texture.getWidth() * N,
+                texture.getHeight() * N);
+
+        Image tiledImage = new Image(tiledRegion);
+        tiledImage.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getWidth());
+
+        tiledImage.setPosition(0,
+                Gdx.graphics.getHeight() - tiledImage.getHeight());
+
+        stage.addActor(tiledImage);
+    }
+}
